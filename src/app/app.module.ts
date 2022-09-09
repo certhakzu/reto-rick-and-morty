9ff7d7aa1fc51b811ee38d5d33b81a9727f81645
Piter Velasquez <pealveco@yahoo.com>
2022-09-09 15:49:38 -0500

    esquemas de componentes header, menu, layout, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonajesComponent } from './components/personajes/personajes.component';
+import { RickandmortyComponent } from './components/rickandmorty/rickandmorty.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonajesComponent,
+    RickandmortyComponent,
+    HeaderComponent,
+    MenuComponent,
+    LayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
